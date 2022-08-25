4c24fa5eebe47c854211a76496e6f4e951bac979
chhak0503 <chhak0503@gmail.com>
2022-08-25 18:51:22 +0900

    2022/08/25 SQL 고급 실습

--- 제4장 SQL 고급.sql
+++ 제4장 SQL 고급.sql
@@ -25,6 +25,7 @@ CREATE TABLE `Sales` (
 	`month`	TINYINT NOT NULL,
 	`sale`	INT NOT NULL
 );
+
 #실습 4-2
 INSERT INTO `Member` VALUES ('a101', '박혁거세', '010-1234-1001', '부장', 101, NOW());
 INSERT INTO `Member` VALUES ('a102', '김유신',   '010-1234-1002', '차장', 101, NOW());
@@ -81,13 +82,80 @@ INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a103', 2020, 2,  9
 INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a104', 2020, 2,  84000);
 INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a105', 2020, 2, 180000);
 INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a108', 2020, 2,  76000);
+
 #실습 4-3
+SELECT * FROM `Member` WHERE `name`='김유신';
+SELECT * FROM `Member` WHERE `pos`='차장' AND dep=101;
+SELECT * FROM `Member` WHERE `pos`='차장' OR dep=101;
+SELECT * FROM `Member` WHERE `name` != '김춘추';
+SELECT * FROM `Member` WHERE `name` <> '김춘추';
+SELECT * FROM `Member` WHERE `pos`='사원' OR `pos`='대리';
+SELECT * FROM `Member` WHERE `pos` IN('사원', '대리');
+SELECT * FROM `Member` WHERE `dep` IN(101, 102, 103);
+SELECT * FROM `Member` WHERE `name` LIKE '%신';
+SELECT * FROM `Member` WHERE `name` LIKE '김%';
+SELECT * FROM `Member` WHERE `name` LIKE '김_ _';
+SELECT * FROM `Member` WHERE `name` LIKE '_성_';
+SELECT * FROM `Member` WHERE `name` LIKE '정_';
+SELECT * FROM `Sales` WHERE `sale` > 50000;
+SELECT * FROM `Sales` WHERE `sale` >= 50000 AND `sale` < 100000 AND `month`=1;
+SELECT * FROM `Sales` WHERE `sale` BETWEEN 50000 AND 100000;
+SELECT * FROM `Sales` WHERE `sale` NOT BETWEEN 50000 AND 100000;
+SELECT * FROM `Sales` WHERE `year` IN(2020);
+SELECT * FROM `Sales` WHERE `month` IN(1, 2);
+
 #실습 4-4
+SELECT * FROM `Sales` ORDER BY `sale`;
+SELECT * FROM `Sales` ORDER BY `sale` ASC;
+SELECT * FROM `Sales` ORDER BY `sale` DESC;
+SELECT * FROM `Member` ORDER BY `name`;
+SELECT * FROM `Member` ORDER BY `name` DESC;
+SELECT * FROM `Member` ORDER BY `rdate` ASC;
+SELECT * FROM `Sales` WHERE `sale` > 50000 ORDER BY `sale` DESC;
+SELECT * FROM `Sales` 
+WHERE `sale` > 50000 
+ORDER BY `year`, `month`, `sale` DESC;
+
 #실습 4-5
+SELECT * FROM `Sales` LIMIT 3;
+SELECT * FROM `Sales` LIMIT 0, 3;
+SELECT * FROM `Sales` LIMIT 1, 2;
+SELECT * FROM `Sales` LIMIT 5, 3;
+SELECT * FROM `Sales` ORDER BY `sale` DESC LIMIT 3, 5;
+SELECT * FROM `Sales` WHERE `sale` < 50000 ORDER BY `sale` DESC LIMIT 3;
+SELECT * FROM `Sales`
+WHERE `sale` > 50000
+ORDER BY `year` DESC, `month`, `sale` DESC
+LIMIT 5;
+
 #실습 4-6
+SELECT SUM(sale) AS `합계` FROM `Sales`;
+SELECT AVG(sale) AS `평균` FROM `Sales`;
+SELECT MAX(sale) AS `최대값` FROM `Sales`;
+SELECT MIN(sale) AS `최소값` FROM `Sales`;
+SELECT COUNT(`seq`) AS `갯수` FROM `Sales`;
+SELECT COUNT(*) AS `갯수` FROM `Sales`;
+
+select substring(`hp`, 10, 4) as `휴대폰 끝자리` from `Member`;
+
+insert into `Member` values ('b101', '을지문덕', '010-5555-1234', '사장', 107, NOW());
+
 #실습 4-7
+select sum(`sale`) as `2018년도 1월 매출총합` from `Sales` where `year`=2018 and `month`=1;
+
 #실습 4-8
+select 
+	sum(`sale`) as `총합`,
+    avg(`sale`) as `평균`
+from `Sales` 
+where `year`=2019 and `month`=2 and `sale` >= 50000;
+
 #실습 4-9
+select 
+	min(`sale`) as `최저`,
+    max(`sale`) as `최고`
+from `Sales` where `year`=2020;
+
 #실습 4-10
 #실습 4-11
 #실습 4-12
